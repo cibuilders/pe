a2543c9322ce2ec544409e9df4ddd5d95cb4625c
G. // <kiritoclasher@gmail.com>
2019-09-23 03:51:21 +0700

    Change kernel

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ get_sources() {
   repo sync -c --no-tags --no-clone-bundle -j8 -q
   git clone https://github.com/PixelExperience-Devices/device_xiaomi_kenzo.git -b pie device/xiaomi/kenzo
   git clone https://github.com/baalajimaestro/msm8956-common.git -b pie device/xiaomi/msm8956-common
-  git clone https://github.com/baalajimaestro/android_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956 --depth=1
+  git clone https://github.com/AnggaR96s/android_kernel_xiaomi_msm8956-1.git -b pie kernel/xiaomi/msm8956 --depth=1
   git clone https://github.com/baalajimaestro/vendor_xiaomi_kenzo.git -b pie vendor/xiaomi --depth=1
 
   cd ..
